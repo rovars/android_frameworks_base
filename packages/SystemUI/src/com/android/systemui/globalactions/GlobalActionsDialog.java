cbef267dde72ed99aacbebbeee406df69c71bf3a
Vachounet <vachounet@live.fr>
2020-10-14 11:43:10 +0200

    UI: Remove black background from power menu
    Change-Id: I87092d57ea457e6d6783c87b8a64b4e69dec2d8a

--- packages/SystemUI/src/com/android/systemui/globalactions/GlobalActionsDialog.java
+++ packages/SystemUI/src/com/android/systemui/globalactions/GlobalActionsDialog.java
@@ -2707,7 +2707,7 @@ public class GlobalActionsDialog implements DialogInterface.OnDismissListener,
             initializeWalletView();
             if (mBackgroundDrawable == null) {
                 mBackgroundDrawable = new ScrimDrawable();
-                mScrimAlpha = 1.0f;
+                mScrimAlpha = 0.45f;
             }
             getWindow().setBackgroundDrawable(mBackgroundDrawable);
         }
@@ -2744,7 +2744,6 @@ public class GlobalActionsDialog implements DialogInterface.OnDismissListener,
             if (!(mBackgroundDrawable instanceof ScrimDrawable)) {
                 return;
             }
-            ((ScrimDrawable) mBackgroundDrawable).setColor(Color.BLACK, animate);
             View decorView = getWindow().getDecorView();
             if (colors.supportsDarkText()) {
                 decorView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_LIGHT_NAVIGATION_BAR |
