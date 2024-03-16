b5c54e033a3632d012566abc2a4a063cb08be8ea
Pranav Vashi <neobuddy89@gmail.com>
2024-03-16 20:01:22 +0530

    Allow signature spoofing on user builds
    * This is custom rom breaking google's integrity check.
      What's point of stopping signature spoofing on user builds?
      Whom you gonna sell? *cough* CyanogenMod *cough*
    
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: SirRGB <sirrgb@proton.me>

--- services/core/java/com/android/server/pm/PackageManagerService.java
+++ services/core/java/com/android/server/pm/PackageManagerService.java
@@ -4424,13 +4424,7 @@ public class PackageManagerService extends IPackageManager.Stub
         return false;
     }
 
-    private static native boolean isDebuggable();
-
     public static boolean isMicrogSigned(AndroidPackage p) {
-        if (!isDebuggable()) {
-            return false;
-        }
-
         // Allowlist the following apps:
         // * com.android.vending - microG Companion
         // * com.google.android.gms - microG Services
